b5f834e476eb56faa1127934c9c674124644e719
itikyu- <en19926@gmail.com>
2014-09-15 23:10:13 +0900

    ファイル構成の見直し gitignoreの作成

---
+++ 01_SystemAdministration/script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+useradd -m -g users -G wheel -s /bin/bash itikyu
+passwd itikyu
--- editor/emacs/.emacs.d/elpa/evil-20130604.821/evil-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "evil" "20130604.821" "Extensible Vi layer for Emacs. [git]"
-  '((undo-tree "0.6.3")))
-
